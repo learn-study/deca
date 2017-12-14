4f3f41b6717b0441454bd364d600b35c5cd28d27
hogehoge <ohayokki@yahoo.co.jp>
2017-12-14 04:34:53 +0000

    acccess

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,9 +3,16 @@ class RelationshipsController < ApplicationController
     @member = Member.find(params[:member_id])
     @relationships = @member.relationships.find_or_initialize_by(member2_id: params[:r_id])
     @relationships.text = params[:relationship][:text] 
-p "#{@relation.inspect}@@@@@@@@@@@@@@@@@"
-    @relationships.save!
+    @relationships.save
     redirect_back(fallback_location: root_path)
+  end
+  
+  def destroy
+    @member = Member.find(params[:r])
+    @relation = @member.relationships.find_by(member2_id: params[:id])
+    @relation.destroy
+    redirect_back(fallback_location: root_path)
+   
   end
   private
   def relation_params
